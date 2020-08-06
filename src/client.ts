2f12685b4e59de0cebd3cce8d5694403cbdd12a6
Juan Mora <juanvalentin.mora@globant.com>
2020-08-06 11:46:34 -0400

    Cambios en configuracion de mongo

--- src/client.ts
+++ src/client.ts
@@ -2,14 +2,22 @@ import { MongoClient, Db } from 'mongodb';
 
 import { mongo } from './config';
 
-const client = new MongoClient(`${mongo.PROTOCOL}://${mongo.HOST}:${mongo.PORT}`, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(`${mongo.PROTOCOL}://${mongo.USER}:${mongo.PASSWORD}@${mongo.HOST}/${mongo.NAMEDB}`, { useNewUrlParser: true, useUnifiedTopology: true });
 
 export let db: Db;
 
 client.connect((err: Error) => {
     console.log("MONGOdb connected");
-    db = client.db("users");
+    db = client.db(mongo.NAMEDB);
     if (err) {
         console.log(err);
     }
 });
+
+
+
+
+
+
+// const uri = "mongodb+srv://raicerk:<password>@laboral-k4r2w.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority";
+
--- src/config.ts
+++ src/config.ts
@@ -5,5 +5,7 @@ export const mongo = {
     PROTOCOL: process.env.MONGOPROTOCOL || 'mongodv+srv',
     PORT: process.env.MONGOPORT || 27017,
     HOST: process.env.MONGOHOST || 'localhost',
-    NAMEDB: process.env.MONGONAMEDB || 'users'
+    NAMEDB: process.env.MONGONAMEDB || 'users',
+    USER: process.env.MONGOUSER || '',
+    PASSWORD: process.env.MONGOPASSWORD || ''
 };
